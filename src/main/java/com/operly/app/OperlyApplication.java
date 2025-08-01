63378322e19194d94a04438ed4ccc609f6c34bc5
Emiliano Daverio <emilianodaverio@gmail.com>
2025-08-01 17:16:23 -0300

    Creacion del proyecto
    Se creo el proyecto con las dependencias necesarias para poder implementar un registro y login con JWT

---
+++ src/main/java/com/operly/app/OperlyApplication.java
@@ -0,0 +1,13 @@
+package com.operly.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/operly/app/OperlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operly.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
